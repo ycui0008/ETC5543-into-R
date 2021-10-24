fe51cb9046fd71451dc22d2475d24fdcd092c428
Yuheng Cui <ycui0008@student.monash.edu>
2021-10-24 22:18:06 +1100

    Writing report - Colour section

--- test/sections/tab-05.R
+++ test/sections/tab-05.R
@@ -1,6 +1,6 @@
 source("questions/q-006.R")
 
-tab05 <- tabPanel("4. Name your plot",
+tab05 <- tabPanel("5. Name your plot",
                   includeHTML("tab05.html"),
                   q6,
                   actionButton("score_btn_3", "Show your scores in this section")
